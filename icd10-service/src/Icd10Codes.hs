b64f9c959d297cee44f6e2b7bdfb8459b677884a
horatius83 <horatius83@gmail.com>
2023-05-22 21:49:09 -0500

    Re-added ICD-10-CM/PCS Order File Format comment

--- icd10-service/src/Icd10Codes.hs
+++ icd10-service/src/Icd10Codes.hs
@@ -38,6 +38,19 @@ Icd10CmPcsOrder
     deriving Show Eq
 |]
 
+{-
+ICD-10-CM/PCS Order File Format
+Position    Length  Contents
+1           5       Order number, right justified, zero filled.
+6           1       Blank
+7           7       ICD-10-CM or ICD-10-PCS code. Dots are not included.
+14          1       Blank
+15          1       0 if the code is a “header” –not valid for HIPAA-covered transactions. 1 if the code is valid for submission for HIPAA-covered transactions.
+16          1       Blank
+17          60      Short description
+77          1       Blank
+78          To end  Long description
+-}
 parseIcd10CmOrder :: String -> Maybe Icd10CmPcsOrder
 parseIcd10CmOrder line = 
     let
