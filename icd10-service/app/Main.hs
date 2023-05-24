780bcdc8a7f7a531cb3d3c85c8a92ad2d749adda
M Monster <horatius83@gmail.com>
2023-05-23 22:02:11 -0500

    It turns out I know less about monads than I thought

--- icd10-service/app/Main.hs
+++ icd10-service/app/Main.hs
@@ -13,36 +13,32 @@ import           Icd10Codes              (getIcd10CodesFromFile, migrateAll)
 main :: IO ()
 main = do
   putStrLn "Opening connection..."
-  let databasePath = pack "test.db"
+  let databasePath = pack "test2.db"
       cmsFilePath = pack "./data/icd10/icd10cm_order_2023.txt"
   loadDatabase databasePath cmsFilePath
   putStrLn "Closing connection."
 
 loadDatabase :: Text -> Text -> IO ()
 loadDatabase dbFilePath cmsDataFilePath =
-  runSqlite dbFilePath $ do runMigration migrateAll
-    -- get row count
-    -- if it is zero then
-        -- parse values from file
-        -- insert into database
-{-
-loadDatabase :: Connection -> String -> IO ()
-loadDatabase conn textFileLocation = do
-    haveTablesBeenCreated <- retrieveIfTableExists conn
-    if not haveTablesBeenCreated then do
-        putStrLn $ "Tables have not been created, reading codes from " ++ textFileLocation
+  runSqlite dbFilePath $ do 
+    runMigration migrateAll
+    count <- rawQuery "select count(1) from Icd10CmPcsOrder" []
+    if count == 0 then do
+        let insertIcd10Codes icd10Codes = do 
+                putStrLn "Inserting codes into the database..."
+                let icd10Codes = fromRight [] eitherIcd10Codes
+                insertMany $ icd10Codes 
+                putStrLn "Finished inserts."
         eitherIcd10Codes <- getIcd10CodesFromFile textFileLocation
-        if isLeft eitherIcd10Codes then putStrLn (fromLeft "" eitherIcd10Codes)
+        insertResults <- insertIcd10Codes <$> eitherIcd10Codes
+
+        if isLeft eitherIcd10Codes then do 
+            putStrLn (fromLeft "" eitherIcd10Codes)
         else do
-            let
-                icd10Codes = fromRight [] eitherIcd10Codes
-                nIcd10Codes = show $ length icd10Codes
-                createIcd = create conn
-            putStrLn $ nIcd10Codes ++ " codes found."
-            putStrLn "Creating table"
-            createTable conn
-            putStrLn "Inserting values"
-            mapM_ createIcd icd10Codes
-            putStrLn "Values inserted"
-    else putStrLn "Tables have been created"
--}
+            putStrLn "Inserting codes into the database..."
+            let icd10Codes = fromRight [] eitherIcd10Codes
+            insertMany $ icd10Codes 
+            putStrLn "Finished inserts."
+    else
+        putStrLn "Else"
+    putStrLn "Done"
