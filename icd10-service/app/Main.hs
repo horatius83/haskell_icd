91ff01c5a7005ae94e2bd62e1384eb007cdca95c
horatius83 <horatius83@gmail.com>
2023-05-27 15:47:07 -0500

    Output is more helpful, can now override default values

--- icd10-service/app/Main.hs
+++ icd10-service/app/Main.hs
@@ -9,21 +9,29 @@ import Database.Persist
 import Database.Persist.Sqlite
 import Icd10Codes (Icd10CmPcsOrder, getIcd10CodesFromFile, migrateAll)
 import System.Directory (doesFileExist)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  putStrLn "Opening connection..."
-  let databasePath = "test2.db"
-      cmsFilePath = "./data/icd10/icd10cm_order_2023.txt"
+  (databasePath, cmsFilePath) <- getSourceFiles "icd10Codes.db"  "./data/icd10/icd10cm_order_2023.txt"
+  putStrLn $ "Checking if " ++ databasePath ++ " exists..."
   doesDbFileExist <- doesFileExist databasePath
   unless doesDbFileExist $ do
-    putStrLn "No database found, parsing CMS file..."
+    putStrLn $ "No database found, parsing CMS file from " ++ cmsFilePath
     eitherIcdCodes <- getIcd10CodesFromFile cmsFilePath
     putStrLn "Inserting into database..."
     case eitherIcdCodes of
       Right icdCodes -> insertCodesIntoDatabase (pack databasePath) icdCodes
       Left e -> putStrLn e
-  putStrLn "Closing connection."
+    putStrLn "Database created."
+
+getSourceFiles :: String -> String -> IO (String, String)
+getSourceFiles defaultDatabasePath defaultCmsFilePath = do
+    args <- getArgs
+    return $ case args of
+        (databasePath:cmsFilePath:_) -> (databasePath, cmsFilePath)
+        (databaseFilePath:_) -> (databaseFilePath, defaultCmsFilePath)
+        _ -> (defaultDatabasePath, defaultCmsFilePath)
 
 insertCodesIntoDatabase :: Text -> [Icd10CmPcsOrder] -> IO ()
 insertCodesIntoDatabase dbFilePath icd10Codes = runSqlite dbFilePath $ do
